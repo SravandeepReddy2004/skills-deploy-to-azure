70d89b05b694535ea68115eae2910a7e900306e6
S Sravandeep Reddy <sravanreddy254@gmail.com>
2025-08-23 16:52:21 +0530

    Update initialize-repository.sh

--- .github/script/initialize-repository.sh
+++ .github/script/initialize-repository.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 
-
 # PURPOSE: This script establishes an initial related history for 
 # all branches. It merges main into all other branches in this repository 
 # while auto-resolving conflicts in favor of main.
